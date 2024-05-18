dc52bad15648676481c804a640e4fc553b2e9e80
Charles <gitauwairimu@gmail.com>
2024-05-18 12:07:30 +0300

    Signal logic, favicon set

--- pesa/signals.py
+++ pesa/signals.py
@@ -17,21 +17,51 @@ def check_payment_status(sender, instance, created, **kwargs):
             # Access set_contribution_amount from the retrieved Case object
             case_contribution_amount = related_case.set_contribution_amount
 
-            if total_paid_for_case < 0 or total_paid_for_case == 0:
+            if total_paid_for_case < case_contribution_amount:
                 instance.status = 'UNPAID'
             elif total_paid_for_case >= case_contribution_amount:
                 instance.status = 'PAID'
-            else:
-                # Existing logic for valid payment amounts
-                if total_paid_for_case > 0 and total_paid_for_case < case_contribution_amount:
-                    instance.status = 'PARTIALLY_PAID'
-                # else:
-                #     instance.status = 'PAID'
+
             instance.save()
         except decimal.InvalidOperation:
             # Handle non-numeric input
             instance.status = 'INVALID'
             instance.save()
+
+
+
+
+
+
+
+# def check_payment_status(sender, instance, created, **kwargs):
+#     from .models import Payment
+#     if created:
+#         try:
+#             # Attempt to convert amount to a decimal for validation
+#             total_paid_for_case = decimal.Decimal(instance.total_paid_for_case)
+
+#             # Access the related Case object using the foreign key
+#             related_case = instance.case_number  # Assuming 'case_number' is the foreign key field
+
+#             # Access set_contribution_amount from the retrieved Case object
+#             case_contribution_amount = related_case.set_contribution_amount
+
+#             if total_paid_for_case < 0 or total_paid_for_case == 0:
+#                 instance.status = 'UNPAID'
+#             elif total_paid_for_case >= case_contribution_amount:
+#                 instance.status = 'PAID'
+#             else:
+#                 # Existing logic for valid payment amounts
+#                 if total_paid_for_case > 0 and total_paid_for_case < case_contribution_amount:
+#                     instance.status = 'PARTIALLY_PAID'
+#                 # else:
+#                 #     instance.status = 'PAID'
+#             instance.save()
+#         except decimal.InvalidOperation:
+#             # Handle non-numeric input
+#             instance.status = 'INVALID'
+#             instance.save()
 
 
 # @receiver(post_save, sender=Payment)
